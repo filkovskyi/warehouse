270149afad49a5f7657b5bb161bc392189a31355
Sergey Filkovskyi <filkovskyi@gmail.com>
2022-06-30 21:31:49 +0300

    feature-mapbox: add custom 3d model and track animation

--- src/mapbox.js
+++ src/mapbox.js
@@ -1,16 +1,90 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXZhbmRyYWdvIiwiYSI6ImNrcG9odnc2eTBscGgzMXA0dTdseHh3Z2oifQ.BcuLTD0qDBvUY1tYyXzDEA';
 
+/* GENERAL */
 const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [30.558074637524186, 50.43031139997038],
+  center: [30.51694655983212, 50.464567272805226],
   zoom: 18,
-  pitch: 30,
-  bearing: 0,
+  pitch: 50,
+  bearing: -10,
   container: 'map',
   antialias: true
 });
 
+/* Add Map Control */
+map.addControl(new mapboxgl.NavigationControl());
+
+
+/* Truck animation START */
+const origin = [30.516896884911148, 50.463493196776];
+const middle = [30.516443781963062, 50.464039519250434];
+const destination = [30.514561707220707, 50.46530180112268];
+const route = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': [origin, middle, destination]
+      }
+    }
+  ]
+};
+
+const point = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'Point',
+        'coordinates': origin
+      }
+    }
+  ]
+};
+// Calculate the distance in kilometers between route start/end point.
+const lineDistance = turf.length(route.features[0]);
+
+const arc = [];
+
+// Number of steps to use in the arc and animation, more steps means
+// a smoother arc and animation, but too many steps will result in a
+// low frame rate
+const steps = 500;
+
+// Draw an arc between the `origin` & `destination` of the two points
+for (let i = 0; i < lineDistance; i += lineDistance / steps) {
+  const segment = turf.along(route.features[0], i);
+  arc.push(segment.geometry.coordinates);
+}
+
+// Update the route with calculated arc coordinates
+route.features[0].geometry.coordinates = arc;
+
+// Used to increment the value of the point measurement against the route.
+let counter = 0;
+
+/* Truck animation END */
+
+
+/* Auto camera rotation */
+// function rotateCamera(timestamp) {
+//   // clamp the rotation between 0 -360 degrees
+//   // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+//   map.rotateTo((timestamp / 1000) % 360, { duration: 0 });
+//   // Request the next frame of the animation.
+//   requestAnimationFrame(rotateCamera);
+// }
+
 map.on('load', () => {
+  // Start the animation.
+
+  /* Auto camera rotation enable */
+  //  rotateCamera(0);
+
   // Insert the layer beneath any symbol layer.
   const layers = map.getStyle().layers;
   const labelLayerId = layers.find(
@@ -57,12 +131,105 @@ map.on('load', () => {
     },
     labelLayerId
   );
+
+  /* ANIMATION */
+  // Add a source and layer displaying a point which will be animated in a circle.
+  map.addSource('route', {
+    'type': 'geojson',
+    'data': route
+  });
+
+  map.addSource('point', {
+    'type': 'geojson',
+    'data': point
+  });
+
+  map.addLayer({
+    'id': 'route',
+    'source': 'route',
+    'type': 'line',
+    'paint': {
+      'line-width': 2,
+      'line-color': '#007cbf'
+    }
+  });
+  map.addLayer({
+    'id': 'point',
+    'source': 'point',
+    'type': 'symbol',
+    'layout': {
+      // This icon is a part of the Mapbox Streets style.
+      // To view all images available in a Mapbox style, open
+      // the style in Mapbox Studio and click the "Images" tab.
+      // To add a new image to the style at runtime see
+      // https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+      'icon-image': 'airport-15',
+      'icon-size': 2,
+      'icon-rotate': ['get', 'bearing'],
+      'icon-rotation-alignment': 'map',
+      'icon-allow-overlap': true,
+      'icon-ignore-placement': true
+    }
+  });
+  
+  function animate() {
+    const start =
+      route.features[0].geometry.coordinates[
+      counter >= steps ? counter - 1 : counter
+      ];
+    const end =
+      route.features[0].geometry.coordinates[
+      counter >= steps ? counter : counter + 1
+      ];
+    if (!start || !end) return;
+
+    // Update point geometry to a new position based on counter denoting
+    // the index to access the arc
+    point.features[0].geometry.coordinates =
+      route.features[0].geometry.coordinates[counter];
+
+    // Calculate the bearing to ensure the icon is rotated to match the route arc
+    // The bearing is calculated between the current point and the next point, except
+    // at the end of the arc, which uses the previous point and the current point
+    point.features[0].properties.bearing = turf.bearing(
+      turf.point(start),
+      turf.point(end)
+    );
+
+    // Update the source with this new data
+    map.getSource('point').setData(point);
+
+    // Request the next frame of animation as long as the end has not been reached
+    if (counter < steps) {
+      requestAnimationFrame(animate);
+    }
+
+    counter = counter + 1;
+  }
+
+  document.getElementById('replay').addEventListener('click', () => {
+    // Set the coordinates of the original point back to origin
+    point.features[0].geometry.coordinates = origin;
+
+    // Update the source layer
+    map.getSource('point').setData(point);
+
+    // Reset the counter
+    counter = 0;
+
+    // Restart the animation
+    animate(counter);
+  });
+
+  // Start the animation
+  animate(counter);
 });
 
+
 // parameters to ensure the model is georeferenced correctly on the map
-const modelOrigin = [30.558074637524186, 50.43031139997038];
+const modelOrigin = [30.516665787360324, 50.46474478061279];
 const modelAltitude = 0;
-const modelRotate = [Math.PI / 2, 0, 0];
+const modelRotate = [Math.PI / 2, Math.PI / 4, 0];
 
 const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
   modelOrigin,
@@ -93,6 +260,14 @@ const customLayer = {
     this.camera = new THREE.Camera();
     this.scene = new THREE.Scene();
 
+    /* Scene lights */
+    const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
+    this.scene.add(ambient)
+
+    const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96)
+    sunLight.position.set(-69, 44, 14)
+    this.scene.add(sunLight)
+
     // create two three.js lights to illuminate the model
     const directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(0, -70, 100).normalize();
@@ -105,9 +280,8 @@ const customLayer = {
     // use the three.js GLTF loader to add the 3D model to the three.js scene
     const loader = new THREE.GLTFLoader();
     loader.load(
-      'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
+      'models/gltf/condo.glb',
       (gltf) => {
-        debugger
         this.scene.add(gltf.scene);
       }
     );
